fc735d79ff83b79048b2e8e96d79e10a367e70c9
kubodera-minden <72605369+kubodera-minden@users.noreply.github.com>
2020-11-24 10:37:39 +0900

    ビューの継承
    layout.blade.phpを作り。
    indexとeditの共通部分を継承した。

--- laravel/resources/views/Article/edit.blade.php
+++ laravel/resources/views/Article/edit.blade.php
@@ -1,3 +1,5 @@
+@extends('article/layout') <!-- layout.blade.phpを継承 -->
+@section('content')
 <head>
   <title>Laravel Sample</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -27,3 +29,4 @@
         </div>
     </div>
 </div>
+@endsection
--- laravel/resources/views/Article/index.blade.php
+++ laravel/resources/views/Article/index.blade.php
@@ -1,7 +1,5 @@
-<head>
-  <title>Laravel Sample</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-</head>
+@extends('article/layout') <!-- layout.blade.phpを継承 -->
+@section('content')
 <div class="container ops-main">
 <div class="row">
   <div class="col-md-12">
@@ -37,3 +35,4 @@
     <div><a href="/article/create" class="btn btn-default">新規作成</a></div>
   </div>
 </div>
+@endsection
