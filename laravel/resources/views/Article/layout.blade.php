bc55428d444f5599afd4c2a4f34bd58c0b8d691c
kubodera-minden <72605369+kubodera-minden@users.noreply.github.com>
2020-11-26 12:24:17 +0900

    ヘッダーを固定

--- laravel/resources/views/Article/layout.blade.php
+++ laravel/resources/views/Article/layout.blade.php
@@ -3,11 +3,11 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="stylesheet" href="/CSS/style.css">
 </head>
-<nav class="navbar navbar-light navbar-fixed-top mb-2">
+<div class="navbar navbar-dark bg-primary navbar-fixed-top">
   <a href="/article" class="navbar-brand">
     <img src="{{ asset('storage/enect_img/enect_logo.png') }}" class="">
   </a>
-</nav>
+</div>
 <div class="body">
   @yield('content')  
 </div>
