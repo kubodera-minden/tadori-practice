e401fe6cc9988780325a0e0d1bd601f4433aae7f
kubodera-minden <72605369+kubodera-minden@users.noreply.github.com>
2020-11-26 16:47:00 +0900

    Articleの各コンポーネントを作成

---
+++ laravel/resources/js/components/article/ArticleForm.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
