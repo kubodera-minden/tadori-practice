40866dcb8a86acc82c598ecc9e2a4bf15696e98f
kubodera-minden <72605369+kubodera-minden@users.noreply.github.com>
2020-11-26 11:55:19 +0900

    CSSファイルを作成→ロゴ配置
    public/CSS/style.css
    全体で使う想定をしているCSSです。

--- laravel/resources/views/Article/layout.blade.php
+++ laravel/resources/views/Article/layout.blade.php
@@ -1,5 +1,13 @@
 <head>
   <title>Laravel Sample</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/CSS/style.css">
 </head>
-@yield('content')
+<nav class="navbar navbar-light navbar-fixed-top mb-2">
+  <a href="/article" class="navbar-brand">
+    <img src="{{ asset('storage/enect_img/enect_logo.png') }}" class="">
+  </a>
+</nav>
+<div class="body">
+  @yield('content')  
+</div>
