2d3f65a350d825806ec4093d524170cbdf71dbff
kubodera-minden <72605369+kubodera-minden@users.noreply.github.com>
2020-12-28 13:21:31 +0900

    FontAwesome導入。footer作成。
    https://qiita.com/algi_nao/items/5d2befae8f367050ae7f

--- laravel/resources/views/Article/layout.blade.php
+++ laravel/resources/views/Article/layout.blade.php
@@ -15,3 +15,19 @@
 <div class="body">
   @yield('content')  
 </div>
+
+<footer>
+  <div class="footer d-flex flex-column">
+    <div class="sns-container row d-flex justify-content-center m-5">
+      <a class="instagram col-1" href="https://www.instagram.com/minden_official/"></a>
+      <a class="facebook col-1" href="https://www.facebook.com/enetomo"></a>
+      <a class="twitter col-1" href="https://twitter.com/minden_PR"></a>
+    </div>
+    <div class="category-container row d-flex justify-content-center mb-5">
+      <a href="/create" class="col-3">新規投稿</a>
+      <a href="/#/products" class="col-3">顔の見える商品</a>
+      <a href="/#/questions" class="col-3">みんなのハテナ</a>
+      <a href="/#/questions/create" class="col-3">質問する</a>
+    </div>
+  </div>
+</footer>
